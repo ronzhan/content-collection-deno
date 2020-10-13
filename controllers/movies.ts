9d92515e43995994d070037384511996d1ad55dd
Ronnie <ronniemail20@gmail.com>
2020-10-12 23:57:04 -0700

    Add movies.ts to include movie data

---
+++ controllers/movies.ts
@@ -0,0 +1,25 @@
+import { Movie } from '../types.ts'
+
+let movies: Movie[] = [
+  {
+    id: "1",
+    name: "Tenet",
+    genre: "Science Fiction",
+    director: "Christopher Nolan",
+    year: 2020
+  },
+  {
+    id: "2",
+    name: "Interstellar",
+    genre: "Science Fiction",
+    director: "Christopher Nolan",
+    year: 2014
+  },
+  {
+    id: "3",
+    name: "The Martian",
+    genre: "Science Fiction",
+    director: "Ridley Scott",
+    year: 2015
+  }
+]
