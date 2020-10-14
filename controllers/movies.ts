3521b3d2f8125e14a687bcb81fe64a92c5cf5d20
Ronnie <ronniemail20@gmail.com>
2020-10-13 23:54:06 -0700

    Add get all movie route

--- controllers/movies.ts
+++ controllers/movies.ts
@@ -1,4 +1,4 @@
-import { Movie } from '../types.ts'
+import { Movie } from "../types.ts";
 
 let movies: Movie[] = [
   {
@@ -6,20 +6,31 @@ let movies: Movie[] = [
     name: "Tenet",
     genre: "Science Fiction",
     director: "Christopher Nolan",
-    year: 2020
+    year: 2020,
   },
   {
     id: "2",
     name: "Interstellar",
     genre: "Science Fiction",
     director: "Christopher Nolan",
-    year: 2014
+    year: 2014,
   },
   {
     id: "3",
     name: "The Martian",
     genre: "Science Fiction",
     director: "Ridley Scott",
-    year: 2015
-  }
-]
+    year: 2015,
+  },
+];
+
+// @desc get all movies
+// @route GET /api/movies
+const getMovies = ({ response }: { response: any }) => {
+  response.body = {
+    success: true,
+    data: movies,
+  };
+};
+
+export { getMovies };
--- routes.ts
+++ routes.ts
@@ -1,10 +1,8 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
-
+import { getMovies } from "./controllers/movies.ts";
 const router = new Router();
 
 // create a route
-router.get("/api", ({ response }: { response: any }) => {
-  response.body = "hello";
-});
+router.get("/api/movies", getMovies);
 
 export default router;
