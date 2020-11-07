421025626503f43439bc584f46d7894cdc07d61e
Ronniegit config --global user.email ronniemail20@gmail.com <ronniemail20@gmail.com>
2020-11-06 23:51:18 -0800

    (Update) add http methods to routes

--- routes.ts
+++ routes.ts
@@ -1,8 +1,13 @@
-import { Router } from "https://deno.land/x/oak/mod.ts";
-import { getMovies, getMovie } from "./controllers/movies.ts";
+import { Router } from 'https://deno.land/x/oak/mod.ts';
+import { getMovies, getMovie } from './controllers/movies.ts';
 const router = new Router();
 
 // create a route
-router.get("/api/movies", getMovies).get("/api/movies/:id", getMovie);
+router
+  .get('/api/movies', getMovies)
+  .get('/api/movies/:id', getMovie)
+  .post('/api/movies', addMovie)
+  .put('/api/movies/:id', updateMovie)
+  .delete('/api/movies/:id', deleteMovie);
 
 export default router;
