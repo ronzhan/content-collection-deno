463b99dcbaf05ca2fc302f75fd837191500237e3
Ronniegit config --global user.email ronniemail20@gmail.com <ronniemail20@gmail.com>
2020-11-08 23:57:48 -0800

    (Update) add addMovie middleware

--- controllers/movies.ts
+++ controllers/movies.ts
@@ -1,25 +1,26 @@
-import { Movie } from "../types.ts";
+import { v4 } from 'https://deno.land/stu/uuid/mod.ts';
+import { Movie } from '../types.ts';
 
 let movies: Movie[] = [
   {
-    id: "1",
-    name: "Tenet",
-    genre: "Science Fiction",
-    director: "Christopher Nolan",
+    id: '1',
+    name: 'Tenet',
+    genre: 'Science Fiction',
+    director: 'Christopher Nolan',
     year: 2020,
   },
   {
-    id: "2",
-    name: "Interstellar",
-    genre: "Science Fiction",
-    director: "Christopher Nolan",
+    id: '2',
+    name: 'Interstellar',
+    genre: 'Science Fiction',
+    director: 'Christopher Nolan',
     year: 2014,
   },
   {
-    id: "3",
-    name: "The Martian",
-    genre: "Science Fiction",
-    director: "Ridley Scott",
+    id: '3',
+    name: 'The Martian',
+    genre: 'Science Fiction',
+    director: 'Ridley Scott',
     year: 2015,
   },
 ];
@@ -35,13 +36,7 @@ const getMovies = ({ response }: { response: any }) => {
 
 // @desc get one movie
 // @route GET /api/movies/:id
-const getMovie = ({
-  params,
-  response,
-}: {
-  params: { id: string };
-  response: any;
-}) => {
+const getMovie = ({ params, response }: { params: { id: string }; response: any }) => {
   const movie: Movie | undefined = movies.find((el) => el.id === params.id);
 
   if (movie) {
@@ -54,7 +49,30 @@ const getMovie = ({
     response.status = 404;
     response.body = {
       success: false,
-      msg: "No movie found",
+      msg: 'No movie found',
+    };
+  }
+};
+
+// @desc add movies
+// @route POST /api/movies
+const addMovie = async ({ request, response }: { request: any; response: any }) => {
+  const body = await request.body();
+
+  if (!request.hasBody) {
+    response.status = 400;
+    response.body = {
+      success: false,
+      msg: 'No data',
+    };
+  } else {
+    const movie: Movie = body.value;
+    movie.id = v4.generate();
+    movies.push(movie);
+    response.status = 201;
+    response.body = {
+      success: true,
+      data: movie,
     };
   }
 };
